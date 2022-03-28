2487cc0b215b73a1ed40e682d31cc8fd8d51babe
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2022-03-28 18:50:25 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run -d -v $PWD/conf:/etc/nginx/conf.d/ -v $PWD/certs:/certs -p 8080:80 -p 8443:443 nginx
+docker run --restart always -d -v $PWD/conf:/etc/nginx/conf.d/ -v $PWD/certs:/certs -p 8080:80 -p 8443:443 nginx
 
